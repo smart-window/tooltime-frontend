874369df07f44d1ad5882da79b7ba238439db106
smart-window <smart.free.dev@gmail.com>
2021-06-25 20:10:57 +0430

    update map

--- src/components/core/Header.vue
+++ src/components/core/Header.vue
@@ -14,20 +14,30 @@
               <i class="fas fa-map"></i>
               <span> Location </span>
             </template>
-            <b-dropdown-item style="width: 500px">
-              <label v-if="user.Servicearea"
-                >{{ user.Servicearea.Location.zip }}, {{ user.Servicearea.Location.city }},
-                {{ user.Servicearea.Location.state }}</label
-              >
-              <GmapMap
-                :center="{ lat: 10, lng: 10 }"
-                :zoom="7"
-                map-type-id="terrain"
-                style="width: 100%; height: 300px"
-              >
-              </GmapMap>
+
+            <b-dropdown-item style="width: 500px" v-b-modal.modal-1>
+              <label v-if="user.Servicearea">
+                {{ user.Servicearea.Location.zip }}, {{ user.Servicearea.Location.city }},
+                {{ user.Servicearea.Location.state }}
+              </label>
             </b-dropdown-item>
           </b-nav-item-dropdown>
+
+          <b-modal size="lg" id="modal-1" title="Location">
+            <GmapMap
+              :center="{ lat: 10, lng: 10 }"
+              :zoom="7"
+              map-type-id="terrain"
+              style="width: 100%; height: 500px"
+            >
+            </GmapMap>
+
+            <template #modal-footer="{ hide }">
+              <b-button size="sm" variant="outline-secondary" @click="hide('forget')">
+                Close
+              </b-button>
+            </template>
+          </b-modal>
 
           <router-link to="/cart" class="nav-link text-primary">
             <i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart ({{ totalCartItems }})
@@ -57,6 +67,11 @@ export default {
   computed: {
     ...mapState(['user', 'orders']),
     ...mapGetters(['totalCartItems']),
+  },
+  data() {
+    return {
+      showMap: false,
+    }
   },
   mounted() {
     console.log(this.user)
