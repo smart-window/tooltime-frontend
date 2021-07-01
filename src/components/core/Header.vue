3fa730f148c7be3c442913e927fc0d2bd4f26fdd
smart-window <smart.free.dev@gmail.com>
2021-07-01 19:14:39 +0430

    format code

--- src/components/core/Header.vue
+++ src/components/core/Header.vue
@@ -27,8 +27,8 @@
             <GmapMap
               style="width: 100%; height: 500px"
               map-type-id="terrain"
-              :zoom="10"
               :center="{ lat: user.Servicearea.latitude, lng: user.Servicearea.longitude }"
+              :zoom="10"
             >
             </GmapMap>
 
