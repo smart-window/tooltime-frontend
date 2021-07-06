6b88ec7c6adc0f306eb82cd2db7d51da1efd7003
smart-window <smart.free.dev@gmail.com>
2021-07-06 05:52:12 +0430

    make map modal

--- src/components/core/Header.vue
+++ src/components/core/Header.vue
@@ -15,34 +15,68 @@
               <span> Location </span>
             </template>
 
-            <b-dropdown-item style="width: 500px" v-b-modal.modal-1>
+            <b-dropdown-item v-b-modal.modal-1>
               <label v-if="user.Servicearea">
                 {{ user.Servicearea.Location.zip }}, {{ user.Servicearea.Location.city }},
-                {{ user.Servicearea.Location.state }}
+                {{ user.Servicearea.Location.state }}, {{ user.Servicearea.Location.hours }},
+                {{ user.Servicearea.Location.phone }}
               </label>
             </b-dropdown-item>
           </b-nav-item-dropdown>
 
-          <b-modal size="lg" id="modal-1" title="Location">
-            <GmapMap
-              style="width: 100%; height: 500px"
-              map-type-id="terrain"
-              :center="{ lat: user.Servicearea.latitude, lng: user.Servicearea.longitude }"
-              :zoom="10"
-            >
-              <GmapMarker
-                :key="index"
-                v-for="(m, index) in [
-                  {
-                    lat: user.Servicearea.latitude,
-                    lng: user.Servicearea.longitude,
-                    label: 'Location',
-                  },
-                ]"
-                :position="m"
-                @click="center = m"
-              />
-            </GmapMap>
+          <b-modal size="xl" id="modal-1" title="Location">
+            <div class="row">
+              <div class="col-6">
+                <b-card no-body>
+                  <b-card-body class="order-list-wrapper">
+                    <h2 class="text-primary mt-4 mb-3">Location Details</h2>
+                    <p>
+                      <span><i class="fas fa-bookmark"></i> </span>&nbsp;&nbsp;
+                      {{ user.Servicearea.Location.name }}
+                    </p>
+                    <p>
+                      <span><i class="fas fa-book"></i> </span>&nbsp;&nbsp;
+                      {{ user.Servicearea.Location.description }}
+                    </p>
+                    <p>
+                      <span> <i class="fas fa-map-marker-alt"></i> </span>&nbsp;&nbsp;
+                      {{ user.Servicearea.Location.zip }}, {{ user.Servicearea.Location.city }},
+                      {{ user.Servicearea.Location.state }}
+                    </p>
+                    <p>
+                      <span><i class="fas fa-phone-alt"></i> </span>&nbsp;&nbsp;
+                      {{ user.Servicearea.Location.phone }}
+                    </p>
+                    <p>
+                      <span> <i class="fas fa-calendar-alt"></i> </span>&nbsp;&nbsp;
+                      {{ user.Servicearea.Location.hours }}
+                    </p>
+                  </b-card-body>
+                </b-card>
+              </div>
+
+              <div class="col-6">
+                <GmapMap
+                  style="width: 100%; height: 500px"
+                  map-type-id="terrain"
+                  :center="{ lat: user.Servicearea.latitude, lng: user.Servicearea.longitude }"
+                  :zoom="10"
+                >
+                  <GmapMarker
+                    :key="index"
+                    v-for="(m, index) in [
+                      {
+                        lat: user.Servicearea.latitude,
+                        lng: user.Servicearea.longitude,
+                        label: 'Location',
+                      },
+                    ]"
+                    :position="m"
+                    @click="center = m"
+                  />
+                </GmapMap>
+              </div>
+            </div>
 
             <template #modal-footer="{ hide }">
               <b-button size="sm" variant="outline-secondary" @click="hide('forget')">
