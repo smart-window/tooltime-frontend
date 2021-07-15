f99448b151fa41a2bab302826a06d0bc3c4b5bb9
smart-window <smart.free.dev@gmail.com>
2021-07-15 04:32:38 +0430

    update card statsu

--- src/pages/BillingPlan.vue
+++ src/pages/BillingPlan.vue
@@ -3,16 +3,15 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
-          <!-- <h1 class="mt-0">Available Plans</h1> -->
           <b-container align-v="center" class="container">
             <div class="mt-5">
               <b-card-group deck class="mb-3">
                 <b-card
                   v-for="plan in plans"
                   v-bind:key="plan.price"
-                  :border-variant="plan.style"
+                  :border-variant="cardStyle(user.priceId, plan.id)"
                   :header="plan.product.name"
-                  :header-bg-variant="plan.style"
+                  :header-bg-variant="cardStyle(user.priceId, plan.id)"
                   header-text-variant="white"
                   align="center"
                   :title="'$' + plan.unit_amount / 100 + '/month'"
@@ -24,11 +23,15 @@
                   <b-card-text> {{ plan.product.description }}<br /><br /> </b-card-text>
                   <br />
                   <b-form v-if="!user.priceId" :action="apiURL" method="POST">
-                    <b-button type="submit" :variant="plan.style">Subscribe</b-button>
+                    <b-button type="submit" :variant="cardStyle(user.priceId, plan.id)"
+                      >Subscribe</b-button
+                    >
                     <input type="hidden" id="basicPrice" :value="plan.id" name="priceId" />
                   </b-form>
                   <b-form v-if="user.priceId == plan.id" :action="apiURL" method="POST">
-                    <b-button type="submit" :variant="plan.style">Cancel</b-button>
+                    <b-button type="submit" :variant="cardStyle(user.priceId, plan.id)"
+                      >Cancel</b-button
+                    >
                     <input type="hidden" id="basicPrice" :value="plan.id" name="priceId" />
                   </b-form>
                   <b-form
@@ -36,7 +39,9 @@
                     :action="apiURL"
                     method="POST"
                   >
-                    <b-button type="submit" :variant="plan.style">Subscribe</b-button>
+                    <b-button type="submit" :variant="cardStyle(user.priceId, plan.id)"
+                      >Subscribe</b-button
+                    >
                     <input type="hidden" id="basicPrice" :value="plan.id" name="priceId" />
                   </b-form>
 
@@ -44,7 +49,6 @@
                 </b-card>
               </b-card-group>
             </div>
-            <!-- card 1 ends here-->
           </b-container>
         </div>
       </div>
@@ -66,36 +70,37 @@ export default {
   },
   computed: {
     ...mapState(['user']),
+
     apiURL() {
       return config.API_URL + '/stripe/create-checkout-session'
     },
   },
   async mounted() {
-    console.log(this.user)
     if (this.$route.query.session_id) {
       this.handleSubmit()
     }
     const config = await api.getConfig()
     this.plans = config.prices.data
-    this.plans.map((plan) => {
-      plan.style = 'primary'
-      return plan
-    })
-    console.log(this.plans)
+
     this.plans.sort(function (a, b) {
       return b.unit_amount - a.unit_amount
     })
     this.plans.reverse()
   },
   methods: {
+    cardStyle(pId, planId) {
+      if (pId == planId) {
+        return 'primary'
+      } else {
+        return 'secondary'
+      }
+    },
     async handleSubmit() {
       try {
         const res = await api.checkoutSession(this.$route.query.session_id)
-        // await api.updateProfile(id, { priceId: res.plan.id })
         this.$store
           .dispatch('UPDATE_PROFILE', { priceId: res.plan.id })
-          .then((res) => {
-            console.log(res)
+          .then(() => {
             alert('Congratuations! You phurchased a plan!')
           })
           .catch(() => {})
