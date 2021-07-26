ce7338f73ca3252851081a41a799e4c2f426cc5d
smart-window <smart.free.dev@gmail.com>
2021-07-25 21:41:33 -0500

    update alert bar for email confirmation

--- src/components/core/Header.vue
+++ src/components/core/Header.vue
@@ -1,116 +1,117 @@
 <template>
   <div>
-    <p v-if="user.status == 'Pending'" class="verify_alert">
-      Please verify your email address. To resent confirmation link click
-      <a href="#" class="resend_btn" @click="resend_code">here</a>
-    </p>
-    <b-navbar
-      :style="user.status == 'Pending' ? 'margin-top: 30px' : ''"
-      class="navbar navbar-expand-lg bg-white fixed-top border-bottom-1 border-primary shadow"
-    >
-      <b-container>
-        <router-link to="/" class="navbar-brand text-primary"> TOOLTIME </router-link>
-        <b-navbar-nav class="ml-auto" v-show="this.$store.state.user.authorized === true">
-          <router-link to="/products" class="nav-link text-primary">
-            <i class="fa fa-home" aria-hidden="true"></i> Products
-          </router-link>
-          <b-nav-item-dropdown>
-            <template slot="button-content">
-              <i class="fas fa-map"></i>
-              <span> Location </span>
-            </template>
+    <div class="d-flex flex-column bg-white fixed-top border-bottom-1 border-primary shadow">
+      <div>
+        <p v-if="user.status == 'Pending'" class="verify_alert">
+          Please verify your email address. To resent confirmation link click
+          <a href="#" class="resend_btn" @click="resend_code">here</a>
+        </p>
+      </div>
+      <b-navbar class="navbar navbar-expand-lg">
+        <b-container>
+          <router-link to="/" class="navbar-brand text-primary"> TOOLTIME </router-link>
+          <b-navbar-nav class="ml-auto" v-show="this.$store.state.user.authorized === true">
+            <router-link to="/products" class="nav-link text-primary">
+              <i class="fa fa-home" aria-hidden="true"></i> Products
+            </router-link>
+            <b-nav-item-dropdown>
+              <template slot="button-content">
+                <i class="fas fa-map"></i>
+                <span> Location </span>
+              </template>
 
-            <b-dropdown-item v-b-modal.modal-1>
-              <label v-if="user.Servicearea">
-                {{ user.Servicearea.Location.zip }}, {{ user.Servicearea.Location.city }},
-                {{ user.Servicearea.Location.state }}, {{ user.Servicearea.Location.hours }},
-                {{ user.Servicearea.Location.phone }}
-              </label>
-            </b-dropdown-item>
-          </b-nav-item-dropdown>
+              <b-dropdown-item v-b-modal.modal-1>
+                <label v-if="user.Servicearea">
+                  {{ user.Servicearea.Location.zip }}, {{ user.Servicearea.Location.city }},
+                  {{ user.Servicearea.Location.state }}, {{ user.Servicearea.Location.hours }},
+                  {{ user.Servicearea.Location.phone }}
+                </label>
+              </b-dropdown-item>
+            </b-nav-item-dropdown>
 
-          <b-modal size="xl" id="modal-1" title="Location">
-            <div class="row">
-              <div class="col-6">
-                <b-card no-body>
-                  <b-card-body class="order-list-wrapper">
-                    <h2 class="text-primary mt-4 mb-3">Location Details</h2>
-                    <p>
-                      <span><i class="fas fa-bookmark"></i> </span>&nbsp;&nbsp;
-                      {{ user.Servicearea.Location.name }}
-                    </p>
-                    <p>
-                      <span><i class="fas fa-book"></i> </span>&nbsp;&nbsp;
-                      {{ user.Servicearea.Location.description }}
-                    </p>
-                    <p>
-                      <span> <i class="fas fa-map-marker-alt"></i> </span>&nbsp;&nbsp;
-                      {{ user.Servicearea.Location.zip }}, {{ user.Servicearea.Location.city }},
-                      {{ user.Servicearea.Location.state }}
-                    </p>
-                    <p>
-                      <span><i class="fas fa-phone-alt"></i> </span>&nbsp;&nbsp;
-                      {{ user.Servicearea.Location.phone }}
-                    </p>
-                    <p>
-                      <span> <i class="fas fa-calendar-alt"></i> </span>&nbsp;&nbsp;
-                      {{ user.Servicearea.Location.hours }}
-                    </p>
-                  </b-card-body>
-                </b-card>
+            <b-modal size="xl" id="modal-1" title="Location">
+              <div class="row">
+                <div class="col-6">
+                  <b-card no-body>
+                    <b-card-body class="order-list-wrapper">
+                      <h2 class="text-primary mt-4 mb-3">Location Details</h2>
+                      <p>
+                        <span><i class="fas fa-bookmark"></i> </span>&nbsp;&nbsp;
+                        {{ user.Servicearea.Location.name }}
+                      </p>
+                      <p>
+                        <span><i class="fas fa-book"></i> </span>&nbsp;&nbsp;
+                        {{ user.Servicearea.Location.description }}
+                      </p>
+                      <p>
+                        <span> <i class="fas fa-map-marker-alt"></i> </span>&nbsp;&nbsp;
+                        {{ user.Servicearea.Location.zip }}, {{ user.Servicearea.Location.city }},
+                        {{ user.Servicearea.Location.state }}
+                      </p>
+                      <p>
+                        <span><i class="fas fa-phone-alt"></i> </span>&nbsp;&nbsp;
+                        {{ user.Servicearea.Location.phone }}
+                      </p>
+                      <p>
+                        <span> <i class="fas fa-calendar-alt"></i> </span>&nbsp;&nbsp;
+                        {{ user.Servicearea.Location.hours }}
+                      </p>
+                    </b-card-body>
+                  </b-card>
+                </div>
+
+                <div class="col-6">
+                  <GmapMap
+                    style="width: 100%; height: 500px"
+                    map-type-id="terrain"
+                    :center="{ lat: user.Servicearea.latitude, lng: user.Servicearea.longitude }"
+                    :zoom="10"
+                  >
+                    <GmapMarker
+                      :key="index"
+                      v-for="(m, index) in [
+                        {
+                          lat: user.Servicearea.latitude,
+                          lng: user.Servicearea.longitude,
+                          label: 'Location',
+                        },
+                      ]"
+                      :position="m"
+                      @click="center = m"
+                    />
+                  </GmapMap>
+                </div>
               </div>
 
-              <div class="col-6">
-                <GmapMap
-                  style="width: 100%; height: 500px"
-                  map-type-id="terrain"
-                  :center="{ lat: user.Servicearea.latitude, lng: user.Servicearea.longitude }"
-                  :zoom="10"
-                >
-                  <GmapMarker
-                    :key="index"
-                    v-for="(m, index) in [
-                      {
-                        lat: user.Servicearea.latitude,
-                        lng: user.Servicearea.longitude,
-                        label: 'Location',
-                      },
-                    ]"
-                    :position="m"
-                    @click="center = m"
-                  />
-                </GmapMap>
-              </div>
-            </div>
+              <template #modal-footer="{ hide }">
+                <b-button size="sm" variant="outline-secondary" @click="hide('forget')">
+                  Close
+                </b-button>
+              </template>
+            </b-modal>
 
-            <template #modal-footer="{ hide }">
-              <b-button size="sm" variant="outline-secondary" @click="hide('forget')">
-                Close
-              </b-button>
-            </template>
-          </b-modal>
-
-          <router-link to="/billing-plan" class="nav-link text-primary">
-            <i class="fa fa-credit-card" aria-hidden="true"></i> Billing Plan
-          </router-link>
-          <router-link to="/cart" class="nav-link text-primary">
-            <i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart ({{ totalCartItems }})
-          </router-link>
-          <router-link to="/order" class="nav-link text-primary">
-            <i class="fas fa-bookmark"> </i>
-            Orders({{ orders.length }})
-          </router-link>
-          <b-nav-item-dropdown right>
-            <template slot="button-content">
-              <i class="fas fa-user-circle"></i>
-              <span> {{ user.name }}</span>
-            </template>
-            <b-dropdown-item v-on:click="handleSignOut">Sign Out</b-dropdown-item>
-            <b-dropdown-item href="/profile">Profile</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-container>
-    </b-navbar>
+            <router-link to="/billing-plan" class="nav-link text-primary">
+              <i class="fa fa-credit-card" aria-hidden="true"></i> Billing Plan
+            </router-link>
+            <router-link to="/cart" class="nav-link text-primary">
+              <i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart ({{ totalCartItems }})
+            </router-link>
+            <router-link to="/order" class="nav-link text-primary">
+              <i class="fas fa-bookmark"> </i>
+              Orders({{ orders.length }})
+            </router-link>
+            <b-nav-item-dropdown right>
+              <template slot="button-content">
+                <i class="fas fa-user-circle"></i>
+                <span> {{ user.name }}</span>
+              </template>
+              <b-dropdown-item v-on:click="handleSignOut">Sign Out</b-dropdown-item>
+              <b-dropdown-item href="/profile">Profile</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-container>
+      </b-navbar>
+    </div>
   </div>
 </template>
 
@@ -155,7 +156,6 @@ export default {
   padding-top: 5px;
   height: 30px;
   width: 100%;
-  position: absolute;
   background-color: $primary;
   color: white;
 }
